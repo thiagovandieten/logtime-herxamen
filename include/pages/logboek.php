0b7faab648da64074319ea3db9a6502fb6e5e3a9
Fatih celik <celik75@live.nl>
2015-06-24 13:56:34 +0200

    Afbeelding laten weergeven als die resposive is

--- include/pages/logboek.php
+++ include/pages/logboek.php
@@ -212,7 +212,7 @@ elseif(isset($_POST['excel'])){
 <section class="ac-container container-mob">
     <div>
         <input id="ac-0" name="accordion-1" type="checkbox" />
-        <label class="uren-mob-invullen" for="ac-0"><img src="images/icons/uren-mob.png">Uren invullen</label>
+        <label class="uren-mob-invullen" for="ac-0"><img src="_img/icons/uren-mob.png">Uren invullen</label>
         <article class="ac-small-mob">
 
             <h3>Uren bijwerken</h3>
